053e868d2b38edd0a3ff6aa79386ad3d90d9933b
Orsolya Torokne Bus <orsolyabus@gmail.com>
2020-01-23 11:42:06 -0800

    let users oly crud their own account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :authorize_user!, only: [:show, :edit, :update, :destroy]
+  
   # GET /users/1
   def show
   end
@@ -49,5 +50,11 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password)
+    end
+    
+    def authorize_user!
+      unless can? :crud, @user
+        render :file => "public/401.html", :status => :unauthorized
+      end
     end
 end
