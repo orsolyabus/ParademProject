0bb8d5bded0017858d46ab1cf170f1ac8c525c95
Orsolya Torokne Bus <orsolyabus@gmail.com>
2020-01-24 15:18:59 -0800

    create views and controller actiions for events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,49 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @events = current_user.events
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save
+      redirect_to @event, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Event was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_url, notice: 'Event was successfully deleted.'
+  end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:title, :date, :attendees)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :contacts
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
   resource :sessions, only: [:new, :create, :destroy]
-  
+  resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
