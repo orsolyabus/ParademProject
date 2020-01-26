5d54fb9ee236423b597c8ab450fc8a07feb94b64
Orsolya Torokne Bus <orsolyabus@gmail.com>
2020-01-25 17:56:43 -0800

    add response (yes/no) to RSVP

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,4 +1,15 @@
 class Rsvp < ApplicationRecord
   belongs_to :event
   belongs_to :contact
+  
+  enum response: { no: 0, yes: 1 }
+  
+  validates :response, presence: true
+  validates(
+    :contact_id,
+    uniqueness: {
+      scope: :event_id,
+      message: "already responded"
+    }
+  )
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_221539) do
+ActiveRecord::Schema.define(version: 2020_01_25_232455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_01_24_221539) do
     t.bigint "contact_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "response"
     t.index ["contact_id"], name: "index_rsvps_on_contact_id"
     t.index ["event_id"], name: "index_rsvps_on_event_id"
   end
